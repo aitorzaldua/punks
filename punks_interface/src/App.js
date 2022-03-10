3526fa67f7731b23fca7f0571487b0d108ac5680
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-03-10 19:28:26 +0100

    100322 after wallet

--- punks_interface/src/App.js
+++ punks_interface/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+
 import { Collection, Footer, Header, YourNFT } from './containers';
 import { Navbar, Brand, CreateNFT } from './components';
 
 const App = () => {
+
+
   return (
     <div className='App'>
       <div className='home_navbar'>
--- punks_interface/src/components/navbar/Navbar.jsx
+++ punks_interface/src/components/navbar/Navbar.jsx
@@ -1,9 +1,68 @@
-import React from 'react';
-/* import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'; */
+import React, { useEffect, useState } from 'react';
 import logo from '../../assets/logo_dd.svg';
+import ethLogo from '../../assets/eth_logo.png';
 import './navbar.css'
 
+import useTruncatedAddress from './../../hooks/useTruncateAddress';
+
 const Navbar = () => {
+
+  //Check if wallet
+
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+      } else {
+        console.log("No authorized account found")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  //Connect the wallet
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
+  const truncatedAddress = useTruncatedAddress(currentAccount);
+
+
   return (
     <div className='punks__navbar'>
         <div className='punks__navbar-links_logo'>
@@ -15,7 +74,17 @@ const Navbar = () => {
                 <p><a href="#CreateNft">Create NFT</a></p>
                 <p><a href="#Collection">Collection</a></p>
             </div>
-            <button type='button'>Connect Wallet</button>
+            {!currentAccount && (
+              <button className="button" onClick={connectWallet}>
+                Connect Wallet
+              </button>
+            )}
+            {currentAccount && (
+              <div className='account'>
+                <img src={ethLogo} alt='ethLogo' />
+                <p>{truncatedAddress}</p>
+              </div>
+            )}
         </div>
 
     </div>
